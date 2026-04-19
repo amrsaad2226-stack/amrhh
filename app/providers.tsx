3146f6c240ed3439bdbca7fd748115fe0c29bfd1
amrsaad2226 <amrsaad2226@gmail.com>
2026-04-19 11:09:02 +0000

    fix: Implement dark mode and test

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,24 +1,17 @@
 // app/providers.tsx
 "use client";
 
-import { ThemeProvider } from "next-themes";
-import { useEffect, useState } from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const [mounted, setMounted] = useState(false);
-
-  // لمنع أخطاء الـ Hydration في Next.js
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return <>{children}</>;
-  }
-
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <NextThemesProvider 
+      attribute="class" 
+      defaultTheme="system" 
+      enableSystem 
+      disableTransitionOnChange
+    >
       {children}
-    </ThemeProvider>
+    </NextThemesProvider>
   );
 }
